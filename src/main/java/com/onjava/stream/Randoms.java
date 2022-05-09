e9e9c56b36b5def707a054631e28dd80737d04bd
hemy5 <hl2396953683@163.com>
2022-05-09 10:48:43 +0800

    14章流式编程

---
+++ src/main/java/com/onjava/stream/Randoms.java
@@ -0,0 +1,39 @@
+package com.onjava.stream;
+
+import java.util.Random;
+
+/**
+ * 声明式编程与命令式编程是一种风格的差别，
+ * 声明式编程是声明要做什么，而不是指明每一步该如何做，同函数式编程风格。
+ * 命令式编程是指明每一步该如何做，而不是声明要做什么，理解上会更难。
+ */
+
+public class Randoms {
+
+    public static void main(String[] args) {
+        declarativeProgramming();
+        imperativeProgramming();
+    }
+
+    /**
+     * 获取随机数：声明式编程
+     */
+    public static void declarativeProgramming() {
+        // seed为种子值，即随机数生成器的初始化状态，seed的作用不是用来控制生成随机数的大小，而是制造一种伪随机，在Random()函数中加入了一个固定的seed值之后，每次生成的随机序列都是相同的，即生成的随机序列中的元素值和顺序都是一致的。
+        new Random(47)
+                .ints(5, 20)
+                .distinct()
+                .limit(7)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    /**
+     * 获取随机数：命令式编程
+     *
+     */
+    public static void imperativeProgramming() {
+
+    }
+
+}
---
+++ src/test/java/com/onjava/stream/RandomsTest.java
@@ -0,0 +1,15 @@
+package com.onjava.stream;
+
+import org.junit.jupiter.api.Test;
+
+class RandomsTest {
+
+    @Test
+    void declarativeProgramming() {
+        System.out.println("声明式编程");
+    }
+
+    @Test
+    void imperativeProgramming() {
+    }
+}
